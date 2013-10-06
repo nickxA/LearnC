bc58801b12391e381d107230169f869eb18a6b3b
Nicholas <ngreenw@gmail.com>
2013-10-06 11:17:40 -0600

    Added ex9.c

--- ex8.c
+++ ex8.c
@@ -23,9 +23,13 @@ int main(int argc, char *argv[])
 	printf("The size of name (char[]): %ld\n", 
 			sizeof(name));
 	printf("The number of chars: %ld\n", 
+			sizeof(name) / sizeof(char));
+        printf("The size of full name (char[]): %ld\n", 
+			sizeof(full_name));
+	printf("The number of chars: %ld\n", 
 			sizeof(full_name) / sizeof(char));
 
-	printf("name=\"%s\" and full_name=\"%s\"\n",
+	printf("name=  \"%s\" and full name= \"%s\"\n",
 			name, full_name);
 	return 0;
 }
